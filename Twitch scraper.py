2c140da525f062633e8199f9f334c995b90af355
Basil Benny <85282801+basilbenny1002@users.noreply.github.com>
2025-04-17 22:34:38 +0530

    Update Twitch scraper.py
    Made the entire system multi-threaded to improve efficiency

--- Twitch scraper.py
+++ Twitch scraper.py
@@ -1,29 +1,30 @@
-#necessary imports
 import os
-import pandas
-from functions import get_follower_count, scrape_twitch_about, scrape_twitter_profile, extract_emails, scrape_youtube, get_live_streams, is_valid_email, get_subscriber_count, is_valid_text, get_twitch_game_id
 import pandas as pd
+from functions import get_follower_count, scrape_twitch_about, scrape_twitter_profile, extract_emails, scrape_youtube, get_live_streams, is_valid_email, get_subscriber_count, is_valid_text, get_twitch_game_id
 from tqdm import tqdm
 import logging
 import datetime
 from dotenv import load_dotenv
 import threading
 import queue
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, filename="scraper.log", filemode="a",
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+
 today = datetime.date.today()
 yesterday = today - datetime.timedelta(days=1)
 load_dotenv()
 
-access_token = os.getenv("access_token") #TODO: paste your access token here
-client_id = os.getenv("client_id") #TODO: paste your client_id here
-minimum_follower = 40000
-game_id = "32982" #TODO: paste the game id you want to filter from
-output_file_name = "GTA-V scrapes 02-04-2025.csv" #TODO: file name of the output, make sure to include .csv
-# print(get_twitch_game_id(access_token=access_token, client_id=client_id, game_name="gtav"))
-# raise AttributeError
+access_token = os.getenv("access_token")  # TODO: paste your access token here
+client_id = os.getenv("client_id")  # TODO: paste your client_id here
+minimum_follower = 50000
+game_id = "32399"  # TODO: paste the game id you want to filter from
+output_file_name = "CSGO streamers(17-04-2025)3.csv"  # TODO: file name of the output, make sure to include .csv
 
-streams = get_live_streams(game_id, client_id=client_id, access_token=access_token) #making the api request to get the list of live streamers
+streams = get_live_streams(game_id, client_id=client_id, access_token=access_token)  # making the api request to get the list of live streamers
 
-#Initialising empty lists to store values
+# Initialising empty lists to store values
 username = []
 followers = []
 viewer_count = []
@@ -43,78 +44,94 @@ third_contact_date = []
 replied = []
 classify = []
 interested = []
-previous_data = pandas.read_csv(f"All streamers list.csv")
+previous_data = pd.read_csv(f"All streamers list.csv")
 
 previous_streamers = previous_data['Name'].tolist()
+all_streamers = {"Name": previous_streamers}
+  # TODO uncomment this part to make sure previous streamers thingy is working properly
 print(previous_streamers)
 
 good_streamer_count = 0
 for i in tqdm(range(len(streams)), desc="Collecting streamer list"):
-
     """
     Iterating over the API response and appending details of streamers with more than the specified number of followers to a list
     """
-    # if streams[i]['language'] !="en":
-    #     continue
-    follower = get_follower_count(client_id, access_token, user_id=streams[i]['user_id'])  #function to get follower count
+    follower = get_follower_count(client_id, access_token, user_id=streams[i]['user_id'])  # function to get follower count
     if follower > minimum_follower and streams[i]['user_name'] not in previous_streamers:
-        streamer_info = {"user_name": streams[i]['user_name'], "viewer_count": streams[i]['viewer_count'], #Data format of the appended values
-                         "language": streams[i]['language'], 'game_name': streams[i]['game_name'],
-                         'followers': follower}
+        streamer_info = {
+            "user_name": streams[i]['user_name'],
+            "viewer_count": streams[i]['viewer_count'],
+            "language": streams[i]['language'],
+            'game_name': streams[i]['game_name'],
+            'followers': follower
+        }
         streamers.append(streamer_info)
         previous_streamers.append(streams[i]['user_name'])
-        good_streamer_count += 1
-    if good_streamer_count > 10:
-        break
+    #     good_streamer_count += 1
+    # if good_streamer_count > 10:
+    #     break
 
 complete_streamer_list = {"Name": previous_streamers}
 print(previous_streamers)
 
-
 logging.info("Found %d unique streamers", len(streamers))
 logging.info("Done collecting streamers with more than %d followers", minimum_follower)
 logging.info("Collecting other info")
-for i in tqdm(range(len(streamers)), desc="Getting more info"):
-    """
-    Looping over the chosen streamers to get additional info
-    """
-    if not is_valid_text(streamers[i]['user_name']):
-        continue
-    #Initializing empty lists to store different links and sets to prevent duplicate links
+results_queue = queue.Queue()
+
+def process_streamer(streamer, index):
+    if not is_valid_text(streamer['user_name']):
+        logging.warning(f"Invalid username: {streamer['user_name']}")
+        return
+
+    # Initialize data containers
     yt_links = set()
     dc_links = []
     twitter_links = []
     mails_found = set()
-    #appending values
-    username.append(streamers[i]['user_name'])
-    followers.append(streamers[i]['followers'])
-    viewer_count.append(streamers[i]['viewer_count'])
-    language.append(streamers[i]['language'])
-    game_name.append(streamers[i]['game_name'])
-    response = scrape_twitch_about(f"https://www.twitch.tv/{streamers[i]['user_name']}/about") #Scraping their twitch about section
-    socials = response.get('links', [])
-    mail = response.get('emails', [])
-    mails_found.update(mail)
-    emailed.append("No")
-    second_f.append("No")
-    third_follow_up.append("No")
-    initial_contact_date.append("Null")
-    second_contact_date.append("Null")
-    third_contact_date.append("Null")
-    replied.append("Null")
-    classify.append("Null")
-    interested.append("Null")
-    if len(socials) == 0: #checking the absence of any socials
-        discord.append("Couldn't find discord")
-        youtube.append("Couldn't find youtube")
-        subscriber_count.append(0)
-        if len(mails_found) > 0:
-            gmail.append(', '.join(str(element).lower() for element in mails_found))
-            continue
-        else:
-            gmail.append("Couldn't find a valid mail")
-            continue
-    #Collecting socials
+
+    # Collect basic info
+    result = {
+        'username': streamer['user_name'],
+        'followers': streamer['followers'],
+        'viewer_count': streamer['viewer_count'],
+        'language': streamer['language'],
+        'game_name': streamer['game_name'],
+        'emailed': 'No',
+        'second_f': 'No',
+        'third_follow_up': 'No',
+        'initial_contact_date': 'Null',
+        'second_contact_date': 'Null',
+        'third_contact_date': 'Null',
+        'replied': 'Null',
+        'classify': 'Null',
+        'interested': 'Null',
+        'discord': "Couldn't find discord",
+        'youtube': "Couldn't find youtube",
+        'subscriber_count': 0,
+        'gmail': "Couldn't find a valid mail"
+    }
+
+    try:
+        response = scrape_twitch_about(f"https://www.twitch.tv/{streamer['user_name']}/about")
+        if not isinstance(response, dict):
+            logging.error(f"Invalid response type for {streamer['user_name']}: {type(response)}")
+            results_queue.put(result)
+            return
+        socials = response.get('links', [])
+        mail = response.get('emails', [])
+        mails_found.update(mail)
+    except Exception as e:
+        logging.error(f"Error scraping Twitch about for {streamer['user_name']}: {str(e)}")
+        results_queue.put(result)
+        return
+
+    if not socials:
+        result['gmail'] = ", ".join(str(element).lower() for element in mails_found) if mails_found else "Couldn't find a valid mail"
+        results_queue.put(result)
+        return
+
+    # Process social links
     for social_links in socials:
         if "youtube" in str(social_links).lower():
             yt_links.add(social_links)
@@ -123,44 +140,101 @@ for i in tqdm(range(len(streamers)), desc="Getting more info"):
         if "x" in str(social_links).lower() or "twitter" in str(social_links).lower():
             twitter_links.append(social_links)
 
-    if len(yt_links) == 0:
-        youtube.append("Couldn't find youtube")
-        subscriber_count.append(0)
+    if not yt_links:
+        result.update({
+            'youtube': "Couldn't find youtube",
+            'subscriber_count': 0
+        })
     else:
-        youtube.append(", ".join(str(link) for link in yt_links))
-        subs = get_subscriber_count(list(yt_links)[0])
-        if subs:
-            subscriber_count.append(get_subscriber_count(list(yt_links)[0]))
-        else:
-            subscriber_count.append(0)
-    if len(dc_links) == 0:
-        discord.append("Couldn't find discord")
+        result['youtube'] = ", ".join(str(link) for link in yt_links)
+        try:
+            subs = get_subscriber_count(list(yt_links)[0])
+            result['subscriber_count'] = subs if subs else 0
+        except Exception as e:
+            logging.error(f"Error getting YouTube subscriber count for {streamer['user_name']}: {str(e)}")
+            result['subscriber_count'] = 0
+
+    result['discord'] = dc_links[0] if dc_links else "Couldn't find discord"
+
+    
+    if twitter_links:
+        try:
+            twitter_response = scrape_twitter_profile(twitter_links[0])
+            if isinstance(twitter_response, dict) and 'bio' in twitter_response:
+                bio = twitter_response['bio']
+                mail = extract_emails(bio)
+                if mail:
+                    mails_found.update(mail)
+            else:
+                logging.warning(f"Invalid Twitter response for {streamer['user_name']}: {twitter_response}")
+        except Exception as e:
+            logging.error(f"Error scraping Twitter for {streamer['user_name']}: {str(e)}")
+
+    if yt_links:
+        try:
+            youtube_mails = scrape_youtube(yt_links)
+            if youtube_mails:
+                mails_found.update(youtube_mails)
+        except Exception as e:
+            logging.error(f"Error scraping YouTube for {streamer['user_name']}: {str(e)}")
+
+    if not mails_found:
+        result['gmail'] = "Couldn't find a valid gmail"
     else:
-        discord.append(dc_links[0])
+        valid_mails = [i for i in set(mails_found) if is_valid_email(i)]
+        result['gmail'] = ",".join(valid_mails) if valid_mails else "Couldn't find a valid mail"
 
-    if len(twitter_links) > 0:
-        bio = scrape_twitter_profile(twitter_links[0])['bio'] #Scraping twitter bio if present
-        mail = extract_emails(bio)
-        if mail:
-            mails_found.update(mail)
-    if len(yt_links) > 0:
-        mails_found.update(scrape_youtube(yt_links)) #Scraping youtube if present
-    if len(mails_found) == 0:
-        gmail.append("Couldn't find a valid gmail")
-    else:
-        valid_mails = [i for i in set(mails_found) if is_valid_email(i)] #Filters out the invalid mails
-        if valid_mails:
-            gmail.append(",".join([i for i in set(mails_found) if is_valid_email(i)]))
-        else:
-            gmail.append("Couldn't find a valid mail")
-    print(f"mails{mails_found}, subscriber{subscriber_count}, youtube{yt_links}")
+    results_queue.put(result)
 
-#Output structure
-print(len(username), len(followers), len(viewer_count), len(gmail), len(emailed), len(subscriber_count))
-df = pd.DataFrame(complete_streamer_list)
-df.to_csv(f"All streamers list.csv", index=False)
-datas = {"Username": username, "Followers": followers, "Viewer_count": viewer_count, "Language": language,
-         "Game": game_name, "Discord": discord, "Youtube": youtube, "Contact": gmail, "Initial contact": emailed,"Second follow up": second_f, "Third follow up": third_follow_up, "Initial contact date": initial_contact_date, "Second contact date": second_contact_date, "Third contact date": third_contact_date,"Subscriber count": subscriber_count, "Has replied": replied, "Classify": classify, "Interested": interested}
+def main():
+    threads = []
+    for i in tqdm(range(len(streamers)), desc="Getting more info"):
+        thread = threading.Thread(target=process_streamer, args=(streamers[i], i))
+        threads.append(thread)
+        thread.start()
 
-df = pd.DataFrame(datas)
-df.to_csv(path_or_buf=output_file_name, index=False)
+        # Limit number of concurrent threads to prevent overwhelming and ratelimiting the system
+        if len(threads) >= 3:  #number of threads
+            for t in threads:
+                t.join()
+            threads = []
+
+    for t in threads:
+        t.join()
+
+    datas = {
+        'username': [],
+        'followers': [],
+        'viewer_count': [],
+        'language': [],
+        'game_name': [],
+        'discord': [],
+        'youtube': [],
+        'gmail': [],
+        'emailed': [],
+        'second_f': [],
+        'third_follow_up': [],
+        'initial_contact_date': [],
+        'second_contact_date': [],
+        'third_contact_date': [],
+        'subscriber_count': [],
+        'replied': [],
+        'classify': [],
+        'interested': []
+    }
+
+    while not results_queue.empty():
+        result = results_queue.get()
+        for key in datas:
+            datas[key].append(result[key])
+
+    # Save 
+    df = pd.DataFrame(all_streamers)
+    df.to_csv("All streamers list.csv")
+    df = pd.DataFrame(datas)
+    df.to_csv(path_or_buf=output_file_name, index=False)
+    print(f"Processed {len(datas['username'])} streamers")
+
+
+if __name__ == "__main__":
+    main()
